e72671b6e3472d6b1ef5099d5c553bb9838902cd
Lucas Pagadizabal <lucaspagadizabal1996@gmail.com>
2017-08-22 20:03:46 -0300

    Add new Filter | Change Image Default

--- TP1/EntFinalDesarrollo/js/master.js
+++ TP1/EntFinalDesarrollo/js/master.js
@@ -5,12 +5,12 @@
 // binarizacion--
 // brillo--
 // ------
-// saturacion
+// saturacion--
 // suavizado
 // deteccion bordes
 // blur
 var imgOrigin = null;//tomamos la img original
-var srcOrigin = "images/girl.jpg";
+var srcOrigin = "images/paisaje.jpg";
 var ctx = document.getElementById("canvas").getContext("2d");
 var ctrCargaImg = false;
 function loadImgDefault() {
@@ -129,17 +129,6 @@ function brillo() {
          setPixel(imageData,x,y,r,g,b,255);
        }
      }
-  // var pixeles = imageData.data;
-  // var cantPixel = imageData.width * imageData.height;
-  // for (var i = 0; i < cantPixel; i++) {
-  //   // var r = pixeles[i*4];
-  //   // var g = pixeles[i*4 + 1];
-  //   // var b = pixeles[i*4 + 2];
-  //
-  //   pixeles[ i * 4 ] += 90;
-  //   pixeles[ i * 4 + 1 ] += 90;
-  //   pixeles[ i * 4 + 2 ] += 90;
-  // }
      ctx.putImageData(imageData,0,0);
 }
 
@@ -169,3 +158,70 @@ function brillo() {
  function save() {
    ctx.toDataURL("image/png");
  }
+
+ function saturacion() {
+     myDrawImage(imgOrigin);
+   var imageData = ctx.getImageData(0,0,ctx.canvas.width,ctx.canvas.height);
+
+   for (var y = 0; y < imageData.height; y++) {
+        for (var x = 0; x < imageData.width; x++) {
+          var r = getRed(imageData,x,y);
+          var g = getGreen(imageData,x,y);
+          var b = getBlue(imageData,x,y);
+          var hsl = rgbToHsl(r,g,b);//pasaje de rgb a hsl
+          hsl[1] = 0.7;//saturacion de pixel
+          var rgb = hslToRgb(hsl[0],hsl[1],hsl[2]);
+          setPixel(imageData,x,y,rgb[0],rgb[1],rgb[2],255);
+        }
+      }
+      ctx.putImageData(imageData,0,0);
+ }
+
+ function rgbToHsl(r, g, b) {
+   r /= 255, g /= 255, b /= 255;
+
+   var max = Math.max(r, g, b), min = Math.min(r, g, b);
+   var h, s, l = (max + min) / 2;
+
+   if (max == min) {
+     h = s = 0; // achromatic
+   } else {
+     var d = max - min;
+     s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+     switch (max) {
+       case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+       case g: h = (b - r) / d + 2; break;
+       case b: h = (r - g) / d + 4; break;
+     }
+
+     h /= 6;
+   }
+   return [ h, s, l ];
+ }
+
+ function hslToRgb(h, s, l) {
+  var r, g, b;
+
+  if (s == 0) {
+    r = g = b = l; // achromatic
+  } else {
+    function hue2rgb(p, q, t) {
+      if (t < 0) t += 1;
+      if (t > 1) t -= 1;
+      if (t < 1/6) return p + (q - p) * 6 * t;
+      if (t < 1/2) return q;
+      if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+      return p;
+    }
+
+    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    var p = 2 * l - q;
+
+    r = hue2rgb(p, q, h + 1/3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1/3);
+  }
+
+  return [ r * 255, g * 255, b * 255 ];
+}
